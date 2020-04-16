40b893e84b41075a4b1d935910a0494f1d1d6a2c
Lelik <de_al@bk.ru>
2020-04-16 23:14:22 +0300

    fix some bugs

--- app/src/main/java/com/example/a311/MainActivity.java
+++ app/src/main/java/com/example/a311/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,22 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       textViewInputNumbers = findViewById(R.id.textViewInputNumbers);
 
-        textViewInputNumbers = findViewById(R.id.textViewInputNumbers);
+        findViewById(R.id.textViewButton0).setOnClickListener(listener);
+        findViewById(R.id.textViewButton1).setOnClickListener(listener);
+        findViewById(R.id.textViewButton2).setOnClickListener(listener);
+        findViewById(R.id.textViewButton3).setOnClickListener(listener);
+        findViewById(R.id.textViewButton4).setOnClickListener(listener);
+        findViewById(R.id.textViewButton5).setOnClickListener(listener);
+        findViewById(R.id.textViewButton6).setOnClickListener(listener);
+        findViewById(R.id.textViewButton7).setOnClickListener(listener);
+        findViewById(R.id.textViewButton8).setOnClickListener(listener);
+        findViewById(R.id.textViewButton9).setOnClickListener(listener);
+        findViewById(R.id.textViewButtonDot).setOnClickListener(listener);
 
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton0));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton1));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton2));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton3));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton4));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton5));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton6));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton7));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton8));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButton9));
-        setCalcClickListener((TextView) findViewById(R.id.textViewButtonDot));
-
-        TextView textViewButtonC = findViewById(R.id.textViewButtonC);
+       TextView textViewButtonC = findViewById(R.id.textViewButtonC);
         textViewButtonC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,16 +39,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
     }
 
-    private void setCalcClickListener(final TextView button) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                textViewInputNumbers.append(button.getText());
-            }
-        });
-    }
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button button = (Button) v;
+            textViewInputNumbers.append(button.getText());
+        }
+    };
 }
